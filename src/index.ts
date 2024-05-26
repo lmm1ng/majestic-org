bee53b77806de40fb7a8c140a6b4e8e34d65a548
lmm1ng <dei4@yandex.ru>
2024-05-26 10:49:11 +0300

    ems 8 added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js'
 import { Config } from './config'
 import { init } from './core'
-import lspd_8 from './orgs/8/lspd'
-import { CronJob } from 'cron'
+// import lspd_8 from './orgs/8/lspd'
+import ems_8 from './orgs/8/ems'
+// import { CronJob } from 'cron'
 
 const config = new Config()
 
@@ -14,15 +15,15 @@ const client = new Client({
   ],
 })
 
-const orgs = [lspd_8]
+const orgs = [ems_8]
 
 client.once(Events.ClientReady, readyClient => {
   init(readyClient, orgs).catch(() => console.log('Error on init orgs'))
 })
 
-const job = new CronJob('0 */3 * * *', () => {
-  init(client, orgs).catch(() => console.log('Error on init orgs in cron'))
-})
+// const job = new CronJob('0 */3 * * *', () => {
+//   init(client, orgs).catch(() => console.log('Error on init orgs in cron'))
+// })
 
 client.login(config.get('TOKEN'))
-job.start()
+// job.start()
